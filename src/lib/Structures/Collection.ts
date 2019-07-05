b9e5a29c5772d667307acab330440fbb18e6959a
gc <gc@users.noreply.github.com>
2019-07-06 08:11:40 +1000

    licensing

--- src/lib/Structures/Collection.ts
+++ src/lib/Structures/Collection.ts
@@ -1,7 +1,23 @@
+/*
+ * Copyright 2015 - 2018 Amish Shah
+ * Modifications copyright (C) 2013 Vlad Frangu https://github.com/vladfrangu/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /* eslint-disable no-dupe-class-members */
 /**
- * A Map with additional utility methods. This is used throughout discord.js rather than Arrays for anything that has
- * an ID, for significantly improved performance and ease-of-use.
+ * A Map with additional utility methods.
  * @extends {Map}
  */
 class Collection<K, V> extends Map<K, V> {
