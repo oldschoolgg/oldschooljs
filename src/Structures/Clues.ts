b953b062920e954d4c85abb6c74d6f8cfc8294ed
gc <gc@users.noreply.github.com>
2019-12-14 01:56:05 +1100

    fix exports

--- src/Structures/Clues.ts
+++ src/Structures/Clues.ts
@@ -4,12 +4,5 @@ import Medium from './Clues/Medium';
 import Hard from './Clues/Hard';
 import Elite from './Clues/Elite';
 import Master from './Clues/Master';
-import Lucky from './Clues/Lucky';
 
-export { Beginner };
-export { Easy };
-export { Medium };
-export { Hard };
-export { Elite };
-export { Master };
-export { Lucky };
+export { Beginner, Easy, Medium, Hard, Elite, Master };
