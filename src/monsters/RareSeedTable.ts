70a2357fed91383aba17e40fe0d520aabef7d97f
Alexsuperfly <alexcsumner@gmail.com>
2020-01-07 23:28:21 -0500

    Monsters: add RareSeedTable

---
+++ src/monsters/RareSeedTable.ts
@@ -0,0 +1,19 @@
+import LootTable from '../structures/LootTable';
+
+const RareSeedTable = new LootTable()
+	.addItem('Toadflax seed', 1, 47)
+	.addItem('Irit seed', 1, 32)
+	.addItem('Belladonna seed', 1, 31)
+	.addItem('Avantoe seed', 1, 22)
+	.addItem('Poison ivy seed', 1, 22)
+	.addItem('Cactus seed', 1, 21)
+	.addItem('Kwuarm seed', 1, 15)
+	.addItem('Potato cactus seed', 1, 15)
+	.addItem('Snapdragon seed', 1, 10)
+	.addItem('Cadantine seed', 1, 7)
+	.addItem('Lantadyme seed', 1, 5)
+	.addItem('Snape grass seed', 1, 4)
+	.addItem('Dwarf weed seed', 1, 3)
+	.addItem('Torstol seed', 1, 2);
+
+export default RareSeedTable;
