f391c7a1d88e72d4b6b79050c3bdad1c3060451a
gc <gc@users.noreply.github.com>
2020-01-16 05:32:20 +1100

    add men/women

--- src/monsters/index.ts
+++ src/monsters/index.ts
@@ -12,6 +12,8 @@ import GeneralGraardor from './GeneralGraardor';
 import CommanderZilyana from './CommanderZilyana';
 import Kreearra from './Kreearra';
 import KrilTsutsaroth from './KrilTsutsaroth';
+import Man from './Man';
+import Woman from './Woman';
 
 export default {
 	DagannothPrime,
@@ -27,5 +29,7 @@ export default {
 	GeneralGraardor,
 	CommanderZilyana,
 	Kreearra,
-	KrilTsutsaroth
+	KrilTsutsaroth,
+	Man,
+	Woman
 };
