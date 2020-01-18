9cd93131e430301f08f82a33dcf8cc36b7585d13
wyattos <wredmond@eagles.ewu.edu>
2020-01-18 10:37:19 -0800

    Update Monsters.ts

--- test/Monsters.ts
+++ test/Monsters.ts
@@ -464,14 +464,14 @@ test('Callisto', async (test): Promise<void> => {
 		'Chaos rune': 18.28 / 400,
 		'Death rune': 18.28 / 300,
 		'Soul rune': 25.6 / 250,
-		'Cannonball': 32 / 250,
+		Cannonball: 32 / 250,
 		'Uncut ruby': 18.28 / 20,
 		'Uncut diamond': 18.28 / 10,
 		'Mahogany logs': 21.34 / 400,
 		'Limpwurt root': 25.6 / 25,
 		'Magic logs': 25.6 / 100,
 		'Red dragonhide': 42.67 / 75,
-		'Coconut': 64 / 60,
+		Coconut: 64 / 60,
 		'Uncut dragonstone': 64,
 		'Grimy toadflax': 128 / 100,
 		'Dragon pickaxe': 170.67,
@@ -495,12 +495,12 @@ test('Chaos Elemental', async (test): Promise<void> => {
 		'Babydragon bones': 10 / 2,
 		'Bat bones': 10 / 5,
 		'Big bones': 10 / 3,
-		'Bones': 10 / 4,
+		Bones: 10 / 4,
 		'Dragon bones': 10,
 		'Super attack(4)': 10,
 		'Super defence(4)': 10,
 		'Super strength(4)': 10,
-		'Tuna': 10 / 5,
+		Tuna: 10 / 5,
 		'Dragon pickaxe': 256,
 		'Dragon dagger': 11.63,
 		'Dragon 2h sword': 128,
@@ -508,10 +508,10 @@ test('Chaos Elemental', async (test): Promise<void> => {
 		'Blood rune': 12.8 / 75,
 		'Chaos rune': 12.8 / 250,
 		'Death rune': 12.8 / 125,
-		'Mithril dart': 12.8/ 300,
+		'Mithril dart': 12.8 / 300,
 		'Rune arrow': 12.8 / 150,
 		'Clue scroll (elite)': 200,
-		'Pet chaos elemental': 300,
+		'Pet chaos elemental': 300
 	};
 
 	const number = 10_000_000;
@@ -523,10 +523,10 @@ test('Chaos Elemental', async (test): Promise<void> => {
 
 test('Chaos Fanatic', async (test): Promise<void> => {
 	const expectedRates = {
-		'Bones': 1,
+		Bones: 1,
 		'Odium shard 1': 256,
 		'Malediction shard 1': 256,
-		'Battlestaff': 25.6 / 5,
+		Battlestaff: 25.6 / 5,
 		'Splitbark body': 25.6,
 		'Splitbark legs': 25.6,
 		'Zamorak monk top': 32,
@@ -536,8 +536,8 @@ test('Chaos Fanatic', async (test): Promise<void> => {
 		'Smoke rune': 32 / 30,
 		'Chaos rune': 32 / 175,
 		'Blood rune': 32 / 50,
-		'Monkfish': 16 / 3,
-		'Shark': 16,
+		Monkfish: 16 / 3,
+		Shark: 16,
 		'Prayer potion(4)': 16,
 		'Anchovy pizza': 32 / 8,
 		'Grimy lantadyme': 16 / 4,
@@ -547,7 +547,7 @@ test('Chaos Fanatic', async (test): Promise<void> => {
 		'Sinister key': 32,
 		'Pure essence': 64 / 250,
 		'Clue scroll (hard)': 128,
-		'Pet chaos elemental': 1000,
+		'Pet chaos elemental': 1000
 	};
 
 	const number = 10_000_000;
@@ -559,7 +559,7 @@ test('Chaos Fanatic', async (test): Promise<void> => {
 
 test('Crazy Archaeologist ', async (test): Promise<void> => {
 	const expectedRates = {
-		'Bones': 1,
+		Bones: 1,
 		'Odium shard 2': 256,
 		'Malediction shard 2': 256,
 		'Amulet of power': 18.29,
@@ -567,9 +567,9 @@ test('Crazy Archaeologist ', async (test): Promise<void> => {
 		"Red d'hide body": 32,
 		'Rune knife': 32 / 10,
 		'Mud rune': 32 / 30,
-		'Cannonball': 32 / 150,
+		Cannonball: 32 / 150,
 		'Dragon arrow': 128 / 75,
-		'Shark': 16,
+		Shark: 16,
 		'Potato with cheese': 16 / 3,
 		'Prayer potion(4)': 16,
 		'Anchovy pizza': 32 / 8,
@@ -581,7 +581,7 @@ test('Crazy Archaeologist ', async (test): Promise<void> => {
 		'Muddy key': 32,
 		'Onyx bolt tips': 32 / 12,
 		'Long bone': 64,
-		'Fedora': 128,
+		Fedora: 128
 	};
 
 	const number = 10_000_000;
@@ -611,7 +611,7 @@ test('King Black Dragon', async (test): Promise<void> => {
 		'Amulet of power': 18.28,
 		'Dragon javelin heads': 25.6 / 15,
 		'Runite limbs': 32,
-		'Shark': 32 / 4,
+		Shark: 32 / 4,
 		'Kbd heads': 128,
 		'Clue scroll (elite)': 450,
 		'Prince black dragon': 3000,
@@ -642,7 +642,7 @@ test('Scorpia', async (test): Promise<void> => {
 		'Cactus spine': 21.33 / 10,
 		'Admiral pie': 16 / 3,
 		'Prayer potion(4)': 16,
-		'Shark': 16,
+		Shark: 16,
 		'Anchovy pizza': 32 / 8,
 		'Superantipoison(4)': 64,
 		'Phoenix necklace': 18.29,
@@ -672,10 +672,10 @@ test('Venenatis', async (test): Promise<void> => {
 		'Death rune': 18.29 / 300,
 		'Blood rune': 18.29 / 200,
 		'Diamond bolts (e)': 25.6 / 100,
-		'Cannonball': 32 / 250,
+		Cannonball: 32 / 250,
 		'Gold ore': 21.33 / 300,
-		'Onyx bolt tips':  25.6 / 60,
-		'Magic logs':  25.6 / 100,
+		'Onyx bolt tips': 25.6 / 60,
+		'Magic logs': 25.6 / 100,
 		'Limpwurt root': 25.6 / 25,
 		"Red spiders' eggs": 42.67 / 500,
 		'Unicorn horn': 64 / 100,
@@ -683,7 +683,7 @@ test('Venenatis', async (test): Promise<void> => {
 		'Grimy snapdragon': 128 / 100,
 		'Dark crab': 12.8 / 8,
 		'Super restore(4)': 12.8 / 3,
-		'Supercompost': 26.95 / 100,
+		Supercompost: 26.95 / 100,
 		'Antidote++(4)': 42.67 / 10,
 		'Dark fishing bait': 128 / 375,
 		'Yew seed': 128,
@@ -692,7 +692,7 @@ test('Venenatis', async (test): Promise<void> => {
 		'Clue scroll (elite)': 100,
 		'Long bone': 400,
 		'Venenatis spiderling': 2000,
-		'Curved bone': 5013,
+		'Curved bone': 5013
 	};
 
 	const number = 10_000_000;
@@ -701,8 +701,6 @@ test('Venenatis', async (test): Promise<void> => {
 	checkThreshold(test, expectedRates, loot, number);
 	test.end();
 });
-
-
 
 test("Vet'ion", async (test): Promise<void> => {
 	const expectedRates = {
@@ -716,7 +714,7 @@ test("Vet'ion", async (test): Promise<void> => {
 		'Chaos rune': 18.29 / 400,
 		'Death rune': 18.29 / 300,
 		'Blood rune': 18.29 / 200,
-		'Cannonball': 32 / 250,
+		Cannonball: 32 / 250,
 		'Gold ore': 21.33 / 300,
 		'Limpwurt root': 25.6 / 25,
 		'Magic logs': 25.6 / 100,
@@ -727,7 +725,7 @@ test("Vet'ion", async (test): Promise<void> => {
 		'Grimy ranarr weed': 128 / 100,
 		'Sanfew serum(4)': 25.6 / 10,
 		'Ogre coffin key': 25.6 / 10,
-		'Supercompost': 26.95 / 100,
+		Supercompost: 26.95 / 100,
 		'Dark fishing bait': 128 / 375,
 		'Palm tree seed': 128,
 		'Yew seed': 128,
@@ -736,8 +734,7 @@ test("Vet'ion", async (test): Promise<void> => {
 		'Long bone': 400,
 		"Vet'ion jr.": 2000,
 		'Skeleton champion scroll': 5000,
-		'Curved bone': 5013,
-
+		'Curved bone': 5013
 	};
 
 	const number = 10_000_000;
@@ -746,4 +743,3 @@ test("Vet'ion", async (test): Promise<void> => {
 	checkThreshold(test, expectedRates, loot, number);
 	test.end();
 });
-
