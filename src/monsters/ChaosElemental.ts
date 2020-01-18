a59b33c81a457addbc26560bd5da79836a548eff
wyattos <wredmond@eagles.ewu.edu>
2020-01-18 10:07:26 -0800

    yarn lint

--- src/monsters/ChaosElemental.ts
+++ src/monsters/ChaosElemental.ts
@@ -5,29 +5,29 @@ import HerbDropTable from './HerbDropTable';
 
 export const ChaosElementalTable = new LootTable()
 	.tertiary(200, 'Clue scroll (elite)')
-    .tertiary(300, 'Pet chaos elemental')
-    .oneIn(256, 'Dragon pickaxe')
+	.tertiary(300, 'Pet chaos elemental')
+	.oneIn(256, 'Dragon pickaxe')
 
 	/* Weapons and armour */
 	.addItem('Dragon dagger', 1, 11)
-    .addItem('Dragon 2h sword', 1, 1)
+	.addItem('Dragon 2h sword', 1, 1)
 
-    /* Runes and ammunition */
-    .addItem('Air rune', 500, 10)
-    .addItem('Blood rune', 75, 10)
-    .addItem('Chaos rune', 250, 10)
-    .addItem('Death rune', 125, 10)
-    .addItem('Mithril dart', 300, 10)
-    .addItem('Rune arrow', 150, 10)
+	/* Runes and ammunition */
+	.addItem('Air rune', 500, 10)
+	.addItem('Blood rune', 75, 10)
+	.addItem('Chaos rune', 250, 10)
+	.addItem('Death rune', 125, 10)
+	.addItem('Mithril dart', 300, 10)
+	.addItem('Rune arrow', 150, 10)
 
-    /* Other */
-    .addItem('Strange fruit', 10, 13)
-    .addItem('Antidote++(4)', 1, 10)
-    .addItem('Weapon poison(++)', 1, 10)
-    .addItem('Coins', 7500, 8)
+	/* Other */
+	.addItem('Strange fruit', 10, 13)
+	.addItem('Antidote++(4)', 1, 10)
+	.addItem('Weapon poison(++)', 1, 10)
+	.addItem('Coins', 7500, 8)
 
-    /* Subtables */
-    .add(HerbDropTable, 1, 4)
+	/* Subtables */
+	.add(HerbDropTable, 1, 4)
 	.add(RareDropTable, 1, 8)
-	.add(GemTable, 1, 3)
+	.add(GemTable, 1, 3);
 export default new SimpleMonster({ id: 2054, name: 'Chaos Elemental', table: ChaosElementalTable });
--- src/monsters/Venenatis.ts
+++ src/monsters/Venenatis.ts
@@ -1,31 +1,30 @@
 import LootTable from '../structures/LootTable';
 import SimpleMonster from '../structures/SimpleMonster';
 
-
 export const VenenatisTable = new LootTable()
 	.tertiary(100, 'Clue scroll (elite)')
 	.tertiary(2000, 'Venenatis spiderling')
 	.tertiary(400, 'Long bone')
-    .tertiary(5000, 'Curved bone')
-    .oneIn(512, 'Treasonous ring')
+	.tertiary(5000, 'Curved bone')
+	.oneIn(512, 'Treasonous ring')
 
 	/* Weapons and armour */
-    .addItem('Rune pickaxe', 1, 48)
-    .addItem('Rune knife', 60, 20)
-    .addItem('Rune 2h sword', 1, 12)
-    .addItem('Dragon pickaxe', 1, 3)
-    .addItem('Dragon 2h sword', 1, 2)
+	.addItem('Rune pickaxe', 1, 48)
+	.addItem('Rune knife', 60, 20)
+	.addItem('Rune 2h sword', 1, 12)
+	.addItem('Dragon pickaxe', 1, 3)
+	.addItem('Dragon 2h sword', 1, 2)
 
-    /* Runes and ammunition */
+	/* Runes and ammunition */
 
 	.addItem('Chaos rune', 400, 28)
 	.addItem('Death rune', 300, 28)
 	.addItem('Blood rune', 200, 28)
-    .addItem('Diamond bolts (e)', 100, 20)
-    .addItem('Cannonball', 250, 16)
+	.addItem('Diamond bolts (e)', 100, 20)
+	.addItem('Cannonball', 250, 16)
 
-    /* Materials */
-    .addItem(
+	/* Materials */
+	.addItem(
 		[
 			['Uncut ruby', 20],
 			['Uncut diamond', 10]
@@ -33,18 +32,18 @@ export const VenenatisTable = new LootTable()
 		1,
 		28
 	)
-    .addItem('Gold ore', 300, 24)
-    .addItem('Onyx bolt tips', 60, 20)
-    .addItem('Magic logs', 100, 20)
-    .addItem('Limpwurt root', 25, 20)
-    .addItem("Red spiders' eggs", 500, 12)
-    .addItem("Unicorn horn", 100, 8)
-    .addItem("Uncut dragonstone", 1, 8)
-    .addItem("Grimy snapdragon", 100, 4)
+	.addItem('Gold ore', 300, 24)
+	.addItem('Onyx bolt tips', 60, 20)
+	.addItem('Magic logs', 100, 20)
+	.addItem('Limpwurt root', 25, 20)
+	.addItem("Red spiders' eggs", 500, 12)
+	.addItem('Unicorn horn', 100, 8)
+	.addItem('Uncut dragonstone', 1, 8)
+	.addItem('Grimy snapdragon', 100, 4)
 
-    /* Other */
-    .addItem('Coins', [15000,19999], 76)
-    .addItem(
+	/* Other */
+	.addItem('Coins', [15000, 19999], 76)
+	.addItem(
 		[
 			['Dark crab', 8],
 			['Super restore(4)', 3]
@@ -52,11 +51,11 @@ export const VenenatisTable = new LootTable()
 		1,
 		40
 	)
-    .addItem('Supercompost', 100, 19)
-    .addItem('Antidote++(4)', 10, 12)
-    .addItem('Dark fishing bait', 375, 4)
-    .addItem('Yew seed', 1, 4)
-    .addItem('Magic seed', 1, 4)
-    .addItem('Palm tree seed', 1, 4)
+	.addItem('Supercompost', 100, 19)
+	.addItem('Antidote++(4)', 10, 12)
+	.addItem('Dark fishing bait', 375, 4)
+	.addItem('Yew seed', 1, 4)
+	.addItem('Magic seed', 1, 4)
+	.addItem('Palm tree seed', 1, 4);
 
 export default new SimpleMonster({ id: 6610, name: 'Venenatis', table: VenenatisTable });
