d1ea92c9c6ce53763c116a6ac7053277d1b65a89
Alexsuperfly <alexcsumner@gmail.com>
2020-01-19 14:52:10 -0500

    Monsters: add minion drops to Kril table

--- src/monsters/KrilTsutsaroth.ts
+++ src/monsters/KrilTsutsaroth.ts
@@ -2,6 +2,32 @@ import LootTable from '../structures/LootTable';
 import GWRareDropTable, { GWGemTable, ShardTable } from './GWRareDropTable';
 import SimpleMonster from '../structures/SimpleMonster';
 
+const MinionUniqueTable = new LootTable()
+	.add('Coins', [1_300, 1_400], 124)
+	.add('Zamorakian spear', 1, 3);
+
+const MinionShardTable = new LootTable().add('Coins', [1_300, 1_400], 9).add(ShardTable, 1, 3);
+
+const MinionTable = new LootTable()
+	.every('Ashes')
+	.add(MinionUniqueTable, 1, 1)
+	.add(MinionShardTable, 1, 1)
+	.addItem('Steel dart', [95, 100], 8)
+	.addItem('Death rune', [5, 10], 8)
+	.addItem('Blood rune', [5, 10], 8)
+	.addItem('Steel arrow', [95, 100], 7)
+	.addItem('Coins', [1_300, 1_400], 66)
+	.addItem('Shark', 3, 8)
+	.addItem('Tuna potato', 2, 8)
+	.addItem('Wine of zamorak', [5, 10], 8)
+	.addItem('Super attack(3)', 1, 2)
+	.addItem('Super strength(3)', 1, 2)
+	.tertiary(128, 'Clue scroll (hard)');
+
+const ZaklnGritchMinionTable = new LootTable(5000)
+	.every(MinionTable)
+	.addItem('Lesser demon champion scroll', 1, 1);
+
 const UniqueTable = new LootTable()
 	.addItem('Steam battlestaff', 1, 4)
 	.addItem('Zamorakian spear', 1, 4)
@@ -11,6 +37,9 @@ const UniqueTable = new LootTable()
 
 export const KrilTsutsarothTable = new LootTable()
 	.every('Ashes')
+	.every(MinionTable)
+	.every(MinionTable)
+	.every(ZaklnGritchMinionTable)
 	.add(UniqueTable, undefined, 3)
 	.tertiary(250, 'Clue scroll (elite)')
 	.tertiary(5000, `Pet k'ril tsutsaroth`)
