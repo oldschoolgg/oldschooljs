94cf8118cbfa3f3ab746881b8ea9f7cc224f4b5a
gc <gc@users.noreply.github.com>
2020-02-03 09:36:23 +1100

    use Error instead of console

--- src/util/itemID.ts
+++ src/util/itemID.ts
@@ -3,7 +3,7 @@ import Items from '../structures/Items';
 export default function itemID(name: string): number {
 	const item = Items.get(name);
 	if (!item) {
-		throw console.error(`ERROR: ${JSON.stringify(name)} doesnt exist.`);
+		throw new Error(`ERROR: ${JSON.stringify(name)} doesnt exist.`);
 	}
 	return item.id;
 }
