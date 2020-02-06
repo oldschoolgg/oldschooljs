aba928647d36ef1706679343c6df1eccb1fdd423
wyattos <wredmond@eagles.ewu.edu>
2020-02-05 21:13:28 -0800

    subtables and special

---
+++ src/monsters/subtables/NotedHerbTable.ts
@@ -0,0 +1,11 @@
+import LootTable from '../../structures/LootTable';
+
+export default new LootTable()
+
+	.addItem('Grimy kwuarm', 1, 5)
+
+	.addItem('Grimy cadantine', 1, 4)
+
+	.addItem('Grimy dwarf weed', 1, 4)
+
+	.addItem('Grimy lantadyme', 1, 3);
---
+++ src/monsters/subtables/WyvernHerbTable.ts
@@ -0,0 +1,17 @@
+import LootTable from '../../structures/LootTable';
+
+const WyvernHerbTable = new LootTable()
+
+	.addItem('Grimy kwuarm', 2, 260)
+
+	.addItem('Grimy cadantine', 2, 208)
+
+	.addItem('Grimy dwarf weed', 2, 208)
+
+	.addItem('Grimy ranarr weed', 1, 185)
+
+	.addItem('Grimy torstol', 1, 185)
+
+	.addItem('Grimy lantadyme', 2, 155);
+
+export default WyvernHerbTable;
