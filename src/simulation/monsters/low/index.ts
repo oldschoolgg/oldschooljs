352409f930e08724e6fcda2322de6c47232e9eeb
gc <gc@users.noreply.github.com>
2020-03-07 11:27:52 +1100

    re-arrange stuff a bit and add 5 new monsters/tables

---
+++ src/simulation/monsters/low/index.ts
@@ -0,0 +1,19 @@
+import BrineRat from './BrineRat';
+import Goblin from './Goblin';
+import Guard from './Guard';
+import Man from './Man';
+import Woman from './Woman';
+import Ankou from './Ankou';
+import DarkBeast from './DarkBeast';
+import FireGiant from './FireGiant';
+
+export default {
+	BrineRat,
+	Goblin,
+	Guard,
+	Man,
+	Woman,
+	Ankou,
+	DarkBeast,
+	FireGiant
+};
