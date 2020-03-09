976e9befeee2dc34ed3d9893cb0bb997c7e78c69
gc <gc@users.noreply.github.com>
2020-03-10 03:43:13 +1100

    rename guard dog table, drop normal bones

--- src/simulation/monsters/low/GuardDog.ts
+++ src/simulation/monsters/low/GuardDog.ts
@@ -1,11 +1,11 @@
 import LootTable from '../../../structures/LootTable';
 import SimpleMonster from '../../../structures/SimpleMonster';
 
-export const DogTable = new LootTable().every('Bat bones').tertiary(25, 'Ensouled dog head', 1);
+export const GuardDogTable = new LootTable().every('Bones').tertiary(25, 'Ensouled dog head', 1);
 
 export default new SimpleMonster({
 	id: 114,
 	name: 'Guard dog',
-	table: DogTable,
+	table: GuardDogTable,
 	aliases: ['dog']
 });
