3a1e895e5e4c741299628a6ec0d1e1068793bb1a
gc <gc@users.noreply.github.com>
2020-03-10 09:02:36 +1100

    zombie: improve table

--- src/simulation/monsters/low/Zombie.ts
+++ src/simulation/monsters/low/Zombie.ts
@@ -2,7 +2,7 @@ import LootTable from '../../../structures/LootTable';
 import HerbDropTable from '../../subtables/HerbDropTable';
 import SimpleMonster from '../../../structures/SimpleMonster';
 
-export const ZombieTable = new LootTable({ limit: 128 })
+export const ZombieTable = new LootTable()
 	.every('Bones')
 	.tertiary(5000, 'Zombie champion scroll')
 
@@ -20,19 +20,23 @@ export const ZombieTable = new LootTable({ limit: 128 })
 	.add('Steel arrow', 5, 2)
 	.add('Nature rune', 6, 1)
 
-	/* Coins */
-	.add('Coins', 10, 2)
-	.add('Coins', 4, 4)
-	.add('Coins', 18, 3)
-	.add('Coins', 13, 2)
-	.add('Coins', 28, 2)
+	.add(
+		new LootTable()
+			.add('Coins', 10)
+			.add('Coins', 4)
+			.add('Coins', 18)
+			.add('Coins', 13)
+			.add('Coins', 28),
+		1,
+		5
+	)
 
 	/* Other */
 	.add('Fishing bait', 5, 37)
 	.add('Copper ore', 1, 2)
 
 	/* Subtables */
-	.add(HerbDropTable, 1, 10);
+	.add(HerbDropTable, 1, 5);
 
 export default new SimpleMonster({
 	id: 26,
