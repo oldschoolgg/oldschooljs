f38eb346532114df22f792089097ae19dffc2a48
gc <gc@users.noreply.github.com>
2020-03-10 07:00:18 +1100

    fix formatting, use table instead to fix gp rates

--- src/simulation/monsters/low/CaveSlime.ts
+++ src/simulation/monsters/low/CaveSlime.ts
@@ -6,30 +6,33 @@ export const CaveSlimeTable = new LootTable()
 	.every('Swamp tar', [1, 6])
 	.tertiary(128, 'Clue scroll (easy)')
 
-	/* Weapons and Armour */
+	.add(GemTable)
 
+	/* Weapons and Armour */
 	.add('Bronze axe', 1, 16)
 	.add('Iron sword', 1, 12)
 	.add('Bronze full helm', 1, 14)
 	.add('Iron kiteshield', 1, 10)
 	.add('Iron boots')
 
-	/*Runes */
+	/* Runes */
 	.add('Water rune', 15, 20)
 	.add('Earth rune', 5, 7)
 
-	/*Other */
-	.add('Coins', 1, 30)
-	.add('Coins', 4, 27)
-	.add('Coins', 10, 25)
-	.add('Coins', 22, 20)
-	.add('Coins', 46, 13)
+	/* Other */
+	.add(
+		new LootTable()
+			.add('Coins', 1)
+			.add('Coins', 4)
+			.add('Coins', 10)
+			.add('Coins', 22)
+			.add('Coins', 46),
+		1,
+		30
+	)
 	.add('Oil lantern frame', 1, 5)
 	.add('Unlit torch', 1, 21)
-	.add('Gold bar', 1, 2)
-
-	/* Subtables */
-	.add(GemTable);
+	.add('Gold bar', 1, 2);
 
 export default new SimpleMonster({
 	id: 480,
