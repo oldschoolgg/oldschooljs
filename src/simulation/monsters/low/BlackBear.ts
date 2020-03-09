da6d92b2eb689d97548c4d5b80337bbec08d4326
gc <gc@users.noreply.github.com>
2020-03-10 03:31:57 +1100

    rename bear file/able to black bear

--- src/simulation/monsters/low/BlackBear.ts
+++ src/simulation/monsters/low/BlackBear.ts
@@ -1,7 +1,7 @@
 import LootTable from '../../../structures/LootTable';
 import SimpleMonster from '../../../structures/SimpleMonster';
 
-export const BearTable = new LootTable()
+export const BlackBearTable = new LootTable()
 	.every('Bones')
 	.every('Bear fur')
 	.every('Raw bear meat')
@@ -13,6 +13,6 @@ export const BearTable = new LootTable()
 export default new SimpleMonster({
 	id: 2839,
 	name: 'Black bear',
-	table: BearTable,
+	table: BlackBearTable,
 	aliases: ['bear', 'black bear']
 });
