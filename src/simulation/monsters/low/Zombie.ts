5f7f21b0af82e24ef2ed07d834f0bd292b936b38
gc <gc@users.noreply.github.com>
2020-03-10 09:09:33 +1100

    zombie: fix

--- src/simulation/monsters/low/Zombie.ts
+++ src/simulation/monsters/low/Zombie.ts
@@ -2,14 +2,14 @@ import LootTable from '../../../structures/LootTable';
 import HerbDropTable from '../../subtables/HerbDropTable';
 import SimpleMonster from '../../../structures/SimpleMonster';
 
-export const ZombieTable = new LootTable()
+export const ZombieTable = new LootTable({ limit: 128 })
 	.every('Bones')
 	.tertiary(5000, 'Zombie champion scroll')
 
 	/* Weapons and armour */
 	.add('Bronze med helm', 1, 4)
-	.add('Bronze longsword', 1, 1)
-	.add('Iron axe', 1, 1)
+	.add('Bronze longsword')
+	.add('Iron axe')
 
 	/* Runes and ammunition */
 	.add('Iron arrow', 5, 7)
@@ -20,23 +20,19 @@ export const ZombieTable = new LootTable()
 	.add('Steel arrow', 5, 2)
 	.add('Nature rune', 6, 1)
 
-	.add(
-		new LootTable()
-			.add('Coins', 10)
-			.add('Coins', 4)
-			.add('Coins', 18)
-			.add('Coins', 13)
-			.add('Coins', 28),
-		1,
-		5
-	)
+	/* Coins */
+	.add('Coins', 10, 11)
+	.add('Coins', 4, 4)
+	.add('Coins', 18, 3)
+	.add('Coins', 13, 2)
+	.add('Coins', 28, 2)
 
 	/* Other */
 	.add('Fishing bait', 5, 37)
 	.add('Copper ore', 1, 2)
 
 	/* Subtables */
-	.add(HerbDropTable, 1, 5);
+	.add(HerbDropTable, 1, 25);
 
 export default new SimpleMonster({
 	id: 26,
