2af56a8a503c51d0ede5c6e4395c5c15ccbc0ad1
gc <gc@users.noreply.github.com>
2020-03-10 06:56:55 +1100

    use herbdroptable, fix formatting

--- src/simulation/monsters/low/CaveBug.ts
+++ src/simulation/monsters/low/CaveBug.ts
@@ -1,28 +1,16 @@
 import LootTable from '../../../structures/LootTable';
 import SimpleMonster from '../../../structures/SimpleMonster';
+import HerbDropTable from '../../subtables/HerbDropTable';
 
 export const CaveBugTable = new LootTable({ limit: 128 })
-
 	/* Runes and ammunition */
 	.add('Earth rune', [2, 7], 25)
 	.add('Water rune', [8, 119], 25)
 	.add('Nature rune', [1, 2], 7)
 
-	/* Herbs */
-	// TODO - should this use central herb drop table?
-	.add('Grimy guam leaf', 1, 20)
-	.add('Grimy marrentill', 1, 20)
-	.add('Grimy tarromin', 1, 20)
-	.add('Grimy harralander', 1, 6)
-	.add('Grimy ranarr weed', 1, 4)
-	.add('Grimy irit leaf', 1, 4)
-	.add('Grimy kwuarm', 1, 4)
-	.add('Grimy cadantine', 1, 2)
-	.add('Grimy lantadyme', 1, 2)
-	.add('Grimy dwarf weed', 1, 2)
+	.add(HerbDropTable, 1, 25)
 
 	/* Materials */
-
 	.add('Unicorn horn dust', 1, 6)
 	.add('Eye of newt', 1, 8)
 	.add('Limpwurt root', 1, 5)
@@ -30,7 +18,6 @@ export const CaveBugTable = new LootTable({ limit: 128 })
 	.add("Red spiders' eggs", 1, 4)
 
 	/* Other */
-
 	.add('Coins', [3, 8], 30)
 	.add('Tinderbox', 1, 22)
 	.add('Swamp weed', [1, 8], 21)
