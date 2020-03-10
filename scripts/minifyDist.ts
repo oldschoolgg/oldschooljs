3b04a1b637b302680e3ae77b20bdb5b4cb2cb933
gc <gc@users.noreply.github.com>
2020-03-11 07:40:50 +1100

    only minify .json files, remove terser

--- scripts/minifyDist.ts
+++ scripts/minifyDist.ts
@@ -1,6 +1,5 @@
 import { promises as fs } from 'fs';
 import * as path from 'path';
-import { minify } from 'terser';
 
 async function walk(dir: string, fileList: string[] = []): Promise<string[]> {
 	const files = await fs.readdir(dir);
@@ -15,16 +14,6 @@ async function walk(dir: string, fileList: string[] = []): Promise<string[]> {
 export default async function minifyDist(): Promise<void> {
 	const arrayOfPaths = await walk('./dist');
 	for (const path of arrayOfPaths) {
-		if (path.endsWith('.js')) {
-			const codeAsString = await fs.readFile(path).then((file): string => file.toString());
-			const { code, error } = minify(codeAsString);
-			if (error || !code) {
-				console.error(error);
-				process.exit();
-			}
-			await fs.writeFile(path, code);
-		}
-
 		if (path.endsWith('.json')) {
 			const jsonString = await fs.readFile(path).then((file): string => file.toString());
 			const minifiedJson = JSON.stringify(JSON.parse(jsonString));
