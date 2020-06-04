07cca83652c102e426fcb551b1f7e26f1f2b7958
Charalampos Fanoulis <charalampos.fanoulis@gmail.com>
2020-06-04 17:41:35 +0300

    revert: revert changes on scripts/prepare.ts

--- scripts/prepare.ts
+++ scripts/prepare.ts
@@ -3,11 +3,7 @@ import preparePolls from './preparePolls';
 import minifyDist from './minifyDist';
 import prepareItems from './prepareItems';
 
-console.log('preparing news');
 prepareNews();
-console.log('preparing polls');
 preparePolls();
-console.log('minyfying dist');
 minifyDist();
-console.log('preparing items');
 prepareItems();
