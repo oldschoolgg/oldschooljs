51c20914ba9375266635ee44a4e2d836d05d9f64
Andre Da Silva <ard35@njit.edu>
2020-07-18 01:49:00 -0400

    Resolve PR review comments
    - Space loot table according to wiki's loot structure
    - Add bottomless bucket as a oneIn drop in the main table
    - Add Hespori's anima seed table an .every drop in the main table
    - Allow monster to accept farming level in kill() to pass to the function to determine whether or not the user gets tangleroot. 99 farming is the default for simulations.

--- src/simulation/monsters/special/Hespori.ts
+++ src/simulation/monsters/special/Hespori.ts
@@ -9,14 +9,29 @@ const HesporiTable = new LootTable()
 	.add('Iasor seed', [1, 2], 1)
 	.add('Kronos seed', [1, 2], 1);
 
-const OtherTable = new LootTable()
+const MainTable = new LootTable()
+	// Always roll on this table
+	.every(HesporiTable)
+
+	// Pre-roll for bucket
+	.oneIn(35, 'Bottomless compost bucket')
+
+	// Allotment seeds
 	.add('Watermelon seed', [10, 20], 2)
 	.add('Snape grass seed', [6, 16], 2)
+
+	// Flower seeds
 	.add('White lily seed', [8, 18], 5)
 	.add('Limpwurt seed', [6, 14], 2)
+
+	// Hop seeds
 	.add('Wildblood seed', [10, 20], 2)
+
+	// Bush seeds
 	.add('Whiteberry seed', [10, 16], 2)
 	.add('Poison ivy seed', [8, 16], 2)
+
+	// Herb seeds
 	.add('Irit seed', [2, 8], 3)
 	.add('Avantoe seed', [2, 5], 3)
 	.add('Kwuarm seed', [2, 5], 3)
@@ -27,14 +42,20 @@ const OtherTable = new LootTable()
 	.add('Ranarr seed', [1, 2], 2)
 	.add('Snapdragon seed', 1, 2)
 	.add('Torstol seed', 1, 2)
+
+	// Tree seeds
 	.add('Maple seed', [2, 4], 4)
 	.add('Willow seed', [2, 5], 3)
 	.add('Yew seed', 1, 2)
 	.add('Magic seed', 1, 1)
+
+	// Fruit tree seeds
 	.add('Pineapple seed', [3, 6], 3)
 	.add('Papaya tree seed', [1, 3], 3)
 	.add('Palm tree seed', [1, 3], 3)
 	.add('Dragonfruit tree seed', 1, 2)
+
+	// Special seeds
 	.add('Teak seed', [2, 5], 4)
 	.add('Mahogany seed', [1, 3], 3)
 	.add('Cactus seed', [4, 14], 2)
@@ -44,12 +65,13 @@ const OtherTable = new LootTable()
 	.add('Redwood tree seed', 1, 1);
 
 export class Hespori extends Monster {
-    kill(quantity = 1): ItemBank {
+    kill(quantity = 1, options, farmingLevel = 99): ItemBank {
         const loot = new Loot();
         for (let i = 0; i < quantity; i++) {
-			loot.add(HesporiTable.roll());
-            if (roll(35)) loot.add('Bottomless compost bucket');
-			else loot.add(OtherTable.roll());
+			loot.add(MainTable.roll());
+			if (roll(7000 - (farmingLevel * 25))) {
+				loot.add('Tangleroot')
+			}
         }
         return loot.values();
 	}
