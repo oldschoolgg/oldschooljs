9f0889a31bf0cc1cf1aadcdd3c7e71466071e16f
Lajnux <lajnuxxx@gmail.com>
2020-07-30 22:45:18 +0200

    Added zombies, spiders and wolfs

---
+++ src/simulation/monsters/turaelMonsters/UndeadOne.ts
@@ -0,0 +1,14 @@
+import LootTable from '../../../structures/LootTable';
+import SimpleMonster from '../../../structures/SimpleMonster';
+
+//Compensate for not making two different ID monsters
+const ScrollTable = new LootTable().add('Skeletal champion scroll').add('Zombie champion scroll');
+
+export const UndeadOneTable = new LootTable().every('Bones').tertiary(5000, ScrollTable);
+
+export default new SimpleMonster({
+	id: 5342,
+	name: 'Undead one',
+	table: UndeadOneTable,
+	aliases: ['undead one']
+});
