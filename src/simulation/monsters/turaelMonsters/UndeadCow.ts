139aeec6dbdfe5612de32cf9b16aa2a10c1c7ff9
Lajnux <lajnuxxx@gmail.com>
2020-07-30 18:59:48 +0200

    Bears, Dwarfs and Dogs added

---
+++ src/simulation/monsters/turaelMonsters/UndeadCow.ts
@@ -0,0 +1,14 @@
+import LootTable from '../../../structures/LootTable';
+import SimpleMonster from '../../../structures/SimpleMonster';
+
+export const UndeadCowTable = new LootTable()
+	.every('Bones')
+	.every('Cowhide')
+	.every('Raw beef (undead)');
+
+export default new SimpleMonster({
+	id: 2992,
+	name: 'Undead cow',
+	table: UndeadCowTable,
+	aliases: ['undead cow']
+});
