598870cc3d29322b30fe3413177d879f00fd128b
Alexsuperfly <alexcsumner@gmail.com>
2020-07-30 07:55:32 -0400

    openables: update mystery box with new data (#130)

--- src/simulation/openables/MysteryBox.ts
+++ src/simulation/openables/MysteryBox.ts
@@ -3,31 +3,32 @@ import LootTable from '../../structures/LootTable';
 import SimpleOpenable from '../../structures/SimpleOpenable';
 
 const ClueTable = new LootTable()
-	.add('Clue scroll (easy)', 1, 9)
-	.add('Clue scroll (medium)', 1, 4)
-	.add('Clue scroll (hard)', 1, 4);
+	.add('Clue scroll (easy)', 1, 5)
+	.add('Clue scroll (medium)', 1, 3)
+	.add('Clue scroll (hard)', 1, 2);
 
 const MysteryBoxTable = new LootTable()
-	.add('Old boot')
-	.add('Flier')
-	.add('Body rune')
-	.add('Bucket')
+	.oneIn(256, 'Stale baguette')
+
 	.add('Cabbage')
+	.add('Diamond')
+	.add('Bucket')
+	.add('Coins', 500)
+	.add('Flier')
+	.add('Old boot')
+	.add('Body rune')
 	.add('Onion')
-	.add('Coins', [500, 3000])
 	.add('Mithril scimitar')
 	.add('Casket')
 	.add('Steel platebody')
-	.add('Diamond')
 	.add('Nature rune', 20)
-	.add(ClueTable)
-	.add(RareDropTable)
-	.oneIn(150, 'Rune sq shield')
-	.oneIn(256, 'Stale baguette');
+
+	.add(ClueTable, 1, 2)
+	.add(RareDropTable);
 
 export default new SimpleOpenable({
 	id: 6199,
 	name: 'Mystery box',
-	aliases: ['mystery box'],
+	aliases: ['mystery box', 'mystery'],
 	table: MysteryBoxTable
 });
