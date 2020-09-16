e4feed4ec2cb7298578c346eda09d84e74cdbedb
Andre Da Silva <57806957+ard35@users.noreply.github.com>
2020-09-16 08:03:48 -0400

    Add Hespori (#124)
    * Add Hespori
    
    Added the Hespori monster and it's loot table
    
    * Spacing changes
    
    * Spacing
    
    * Resolve PR review comments
    
    - Space loot table according to wiki's loot structure
    - Add bottomless bucket as a oneIn drop in the main table
    - Add Hespori's anima seed table an .every drop in the main table
    - Allow monster to accept farming level in kill() to pass to the function to determine whether or not the user gets tangleroot. 99 farming is the default for simulations.
    
    * Resolve Comments, Lint Code
    
    * Hopefully fix implicity type error
    
    * Add farmingLevel to MonsterKillOptions
    
    * Add default as 99 farming when simulating kills
    
    * Resolve PR review comments
    
    - Space loot table according to wiki's loot structure
    - Add bottomless bucket as a oneIn drop in the main table
    - Add Hespori's anima seed table an .every drop in the main table
    - Allow monster to accept farming level in kill() to pass to the function to determine whether or not the user gets tangleroot. 99 farming is the default for simulations.
    
    * Resolve Comments, Lint Code
    
    * Simplify code with nullish coalescing
    
    * Clean up if statement for tangleroot roll
    
    * Resolve review comments

--- src/simulation/monsters/special/index.ts
+++ src/simulation/monsters/special/index.ts
@@ -1,4 +1,5 @@
 import Barrows from './Barrows';
 import TzTokJad from './TzTokJad';
+import Hespori from './Hespori';
 
-export default { Barrows, TzTokJad };
+export default { Barrows, TzTokJad, Hespori };
