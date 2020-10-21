e90573d3e9154fcebe80ec2c5e9e3bffa4bd06ea
QuantumlyTangled <ethoarx@gmail.com>
2020-10-21 21:07:50 +0200

    chore(jest): add/remove deps

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+module.exports = {
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	testRunner: 'jest-circus/runner',
+	testMatch: ['<rootDir>/tests/**/*.test.ts'],
+	globals: {
+		'ts-jest': {
+			tsConfig: '<rootDir>/tests/tsconfig.json'
+		}
+	}
+};
