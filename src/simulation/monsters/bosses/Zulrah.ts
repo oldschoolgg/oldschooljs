2f6028176f37235187cd12922df68cd151b0bc58
tlbraams <t.l.braams+rsgit@gmail.com>
2020-11-05 23:29:13 +0100

    Refactor Zulrah LootTable
    Refactor Zulrah lootTable, creating a single table with two rolls on the normal table , a roll for the scales drop and the tertiary rollls.
    This also eliminates the need for the overwritten kill method.

--- src/simulation/monsters/bosses/Zulrah.ts
+++ src/simulation/monsters/bosses/Zulrah.ts
@@ -1,7 +1,7 @@
-import { randInt } from 'e';
+// import { randInt } from 'e';
 
-import { ItemBank } from '../../../meta/types';
-import Bank from '../../../structures/Bank';
+// import { ItemBank } from '../../../meta/types';
+// import Bank from '../../../structures/Bank';
 import LootTable from '../../../structures/LootTable';
 import SimpleMonster from '../../../structures/SimpleMonster';
 import RareDropTable from '../../subtables/RareDropTable';
@@ -69,35 +69,38 @@ const ZulrahTable = new LootTable()
 	.add('Swamp tar', 1000, 5)
 	.add("Zulrah's scales", 500, 5);
 
-const ZulrahTertiaryTable = new LootTable()
+const TotalZulrahyTable = new LootTable()
+	.every(ZulrahTable)
+	.every(ZulrahTable)
+	.every('Zulrah\'s scales', [100,299])
 	.tertiary(75, 'Clue scroll (elite)')
 	.tertiary(3000, 'Jar of swamp')
 	.tertiary(4000, 'Pet snakeling');
 
-export class Zulrah extends SimpleMonster {
-	public kill(quantity = 1): ItemBank {
-		const loot = new Bank();
+// export class Zulrah extends SimpleMonster {
+// 	public kill(quantity = 1): ItemBank {
+// 		const loot = new Bank();
 
-		for (let i = 0; i < quantity; i++) {
-			loot.add(ZulrahTable.roll());
-			loot.add(ZulrahTable.roll());
+// 		for (let i = 0; i < quantity; i++) {
+// 			loot.add(ZulrahTable.roll());
+// 			loot.add(ZulrahTable.roll());
 
-			loot.add("Zulrah's scales", randInt(100, 299));
+// 			loot.add("Zulrah's scales", randInt(100, 299));
 
-			loot.add(ZulrahTertiaryTable.roll());
+// 			loot.add(ZulrahTertiaryTable.roll());
 
-			// if (roll(75)) loot.add('Clue scroll (elite)');
-			// if (roll(3000)) loot.add('Jar of swamp');
-			// if (roll(4000)) loot.add('Pet snakeling');
-		}
+// 			// if (roll(75)) loot.add('Clue scroll (elite)');
+// 			// if (roll(3000)) loot.add('Jar of swamp');
+// 			// if (roll(4000)) loot.add('Pet snakeling');
+// 		}
 
-		return loot.values();
-	}
-}
+// 		return loot.values();
+// 	}
+// }
 
-export default new Zulrah({
+export default new SimpleMonster({
 	id: 2042,
 	name: 'Zulrah',
-	table: new LootTable().add(ZulrahTable).add(ZulrahTertiaryTable),
+	table: TotalZulrahyTable,
 	aliases: ['zulrah', 'snek', 'zul']
 });
