5f3c57b507a8be1762c8a95754abe0afefd76f7f
tlbraams <t.l.braams@student.utwente.nl>
2020-11-05 21:11:45 +0100

    fix ESLint issues
    Remove whitespace indicated by ESLint

--- src/simulation/monsters/bosses/Zulrah.ts
+++ src/simulation/monsters/bosses/Zulrah.ts
@@ -85,7 +85,7 @@ export class Zulrah extends SimpleMonster {
 			loot.add("Zulrah's scales", randInt(100, 299));
 
 			loot.add(ZulrahTertiaryTable.roll());
-			
+
 			// if (roll(75)) loot.add('Clue scroll (elite)');
 			// if (roll(3000)) loot.add('Jar of swamp');
 			// if (roll(4000)) loot.add('Pet snakeling');
