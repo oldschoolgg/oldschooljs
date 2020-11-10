db9e93607340eba325746c53fd250e69207c0836
QuantumlyTangled <ethoarx@gmail.com>
2020-11-10 17:19:59 +0100

    refactor: worlds

--- test/Worlds.test.ts
+++ test/Worlds.test.ts
@@ -1,34 +1,37 @@
 import { Worlds } from '../dist';
 
-test('Setup', async () => {
-	await Worlds.fetch();
-});
+describe('Worlds', () => {
+	beforeAll(async () => {
+		await Worlds.fetch();
+	});
 
-test('Generic Worlds', () => {
-	expect.assertions(2);
+	test('Generic Worlds', () => {
+		expect.assertions(2);
 
-	// New worlds are sometimes added, so just check it has atleast the amount of normal worlds.
-	expect(Worlds.size >= 180).toBeTruthy();
+		// New worlds are sometimes added, so just check it has atleast the amount of normal worlds.
+		expect(Worlds.size >= 180).toBeTruthy();
 
-	// There should be more than 14 Australian Worlds.
-	const australianWorlds = Worlds.filter((world) => world.location === 'Australia');
-	expect(australianWorlds.size > 14).toBeTruthy();
-});
+		// There should be more than 14 Australian Worlds.
+		const australianWorlds = Worlds.filter((world) => world.location === 'Australia');
+		expect(australianWorlds.size > 14).toBeTruthy();
+	});
 
-test('Single world should be correct', (done) => {
-	expect.assertions(3);
+	jest.setTimeout(60000);
+	test('Single world should be correct', () => {
+		expect.assertions(3);
 
-	const WorldOne = Worlds.get(301);
-	if (!WorldOne) throw done.fail('World One not found.');
+		const WorldOne = Worlds.get(301);
+		if (!WorldOne) return fail('World One not found.');
 
-	expect(WorldOne.number).toBe(301);
-	expect(WorldOne.location).toBe('United States');
-	expect(WorldOne.activity).toBe('Trade - Free');
-});
+		expect(WorldOne.number).toEqual(301);
+		expect(WorldOne.location).toEqual('United States');
+		expect(WorldOne.activity).toEqual('Trade - Free');
+	});
 
-test('Getting worlds in different formats', () => {
-	expect.assertions(2);
+	test('Getting worlds in different formats', () => {
+		expect.assertions(2);
 
-	expect(Worlds.get(301)).toBe(Worlds.get(1));
-	expect(Worlds.get(230)).toBe(Worlds.get(530));
+		expect(Worlds.get(301)).toBe(Worlds.get(1));
+		expect(Worlds.get(230)).toBe(Worlds.get(530));
+	});
 });
