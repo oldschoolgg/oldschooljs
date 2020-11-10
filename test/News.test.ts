692e2a667fd416fc8d5cebb6c5035986d1181a2f
QuantumlyTangled <ethoarx@gmail.com>
2020-11-10 17:39:38 +0100

    refactor: news

--- test/News.test.ts
+++ test/News.test.ts
@@ -1,29 +1,33 @@
 import { News } from '../dist';
 
-jest.setTimeout(60000);
-test('Generic checks', async () => {
-	expect.assertions(1);
+describe('News', () => {
+	jest.setTimeout(60000);
+	test('Generic checks', async () => {
+		expect.assertions(1);
 
-	const firstPageNews = News.filter((article) => article.year === 2013 && article.month === 2);
+		const firstPageNews = News.filter(
+			(article) => article.year === 2013 && article.month === 2
+		);
 
-	expect(firstPageNews.size).toBe(12);
-});
+		expect(firstPageNews.size).toEqual(12);
+	});
 
-test('Single article check', (done) => {
-	expect.assertions(4);
+	test('Single article check', (done) => {
+		expect.assertions(4);
 
-	const article = News.find(
-		(article) => article.title === 'GameBlast15 | Friday 12:00 UTC | Live Stream'
-	);
+		const article = News.find(
+			(article) => article.title === 'GameBlast15 | Friday 12:00 UTC | Live Stream'
+		);
 
-	if (!article) return done.fail('Missing article.');
+		if (!article) return done.fail('Missing article.');
 
-	Promise.all([
-		expect(article.category).toBe('Website'),
-		expect(article.link).toBe(
-			'https://secure.runescape.com/m=news/gameblast15--friday-1200-utc--live-stream?oldschool=1'
-		),
-		expect(article.year).toBe(2015),
-		expect(article.month).toBe(2)
-	]).then(() => done());
+		Promise.all([
+			expect(article.category).toEqual('Website'),
+			expect(article.link).toEqual(
+				'https://secure.runescape.com/m=news/gameblast15--friday-1200-utc--live-stream?oldschool=1'
+			),
+			expect(article.year).toEqual(2015),
+			expect(article.month).toEqual(2)
+		]).then(() => done());
+	});
 });
--- test/Polls.test.ts
+++ test/Polls.test.ts
@@ -6,16 +6,16 @@ describe('Polls', () => {
 	test('Generic checks', (done) => {
 		expect.assertions(3);
 
-		expect(Polls.size > 163).toBe(true);
+		expect(Polls.size > 163).toEqual(true);
 
 		const raidsPoll = Polls.find((poll) => poll.title === 'More Raids Rewards');
 		if (!raidsPoll) return done.fail('Expected raids poll.');
-		expect(raidsPoll.questions.length).toBe(11);
+		expect(raidsPoll.questions.length).toEqual(11);
 
 		const pollsFrom2013 = Polls.filter(
 			(poll) => new Date(poll.datePosted).getFullYear() === 2013
 		);
-		expect(pollsFrom2013.size).toBe(23);
+		expect(pollsFrom2013.size).toEqual(23);
 		return done();
 	});
 
