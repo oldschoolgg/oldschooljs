37461cecadf650fea1c88a2322ef2c759fcfed16
QuantumlyTangled <ethoarx@gmail.com>
2020-11-10 19:37:12 +0100

    refactor: bankclass

--- test/BankClass.test.ts
+++ test/BankClass.test.ts
@@ -4,88 +4,90 @@ import LootTable from '../dist/structures/LootTable';
 
 const TestLootTable = new LootTable().add('Toolkit');
 
-test('adding', () => {
-	const bank = new Bank({ 1: 1 });
-	expect(bank.amount(1)).toBe(1);
-	bank.add(1, 1);
-	expect(bank.amount(1)).toBe(2);
-	expect(bank.amount('Toolkit')).toBe(2);
-	expect(bank.amount('Twisted bow')).toBe(0);
-	expect(bank.bank).toEqual({ 1: 2 });
-});
+describe('Bank Class', () => {
+	test('adding', () => {
+		const bank = new Bank({ 1: 1 });
+		expect(bank.amount(1)).toBe(1);
+		bank.add(1, 1);
+		expect(bank.amount(1)).toBe(2);
+		expect(bank.amount('Toolkit')).toBe(2);
+		expect(bank.amount('Twisted bow')).toBe(0);
+		expect(bank.bank).toEqual({ 1: 2 });
+	});
 
-test('removing', () => {
-	const bank = new Bank({ 1: 1 });
-	expect(bank.amount(1)).toBe(1);
-	bank.remove(1, 1);
-	expect(bank.amount(1)).toBe(0);
-	expect(bank.amount('Toolkit')).toBe(0);
+	test('removing', () => {
+		const bank = new Bank({ 1: 1 });
+		expect(bank.amount(1)).toBe(1);
+		bank.remove(1, 1);
+		expect(bank.amount(1)).toBe(0);
+		expect(bank.amount('Toolkit')).toBe(0);
 
-	expect(bank.bank).toEqual({});
-});
+		expect(bank.bank).toEqual({});
+	});
 
-test('chaining', () => {
-	const bank = new Bank().add(1).remove(1).add(1);
+	test('chaining', () => {
+		const bank = new Bank().add(1).remove(1).add(1);
 
-	expect(bank.amount(1)).toBe(1);
-});
+		expect(bank.amount(1)).toBe(1);
+	});
 
-test('multiply', () => {
-	const bank = new Bank({ 1: 1, 2: 1, 3: 1, 4: 1 });
-	bank.multiply(10);
-	expect(bank.amount(1)).toBe(10);
-	expect(bank.amount(2)).toBe(10);
-	expect(bank.amount(3)).toBe(10);
-	expect(bank.amount(4)).toBe(10);
-});
+	test('multiply', () => {
+		const bank = new Bank({ 1: 1, 2: 1, 3: 1, 4: 1 });
+		bank.multiply(10);
+		expect(bank.amount(1)).toBe(10);
+		expect(bank.amount(2)).toBe(10);
+		expect(bank.amount(3)).toBe(10);
+		expect(bank.amount(4)).toBe(10);
+	});
 
-test('random', () => {
-	const bank = new Bank({ 69: 420 });
-	const random = bank.random();
-	expect(random).toEqual({ id: 69, qty: 420 });
-});
+	test('random', () => {
+		const bank = new Bank({ 69: 420 });
+		const random = bank.random();
+		expect(random).toEqual({ id: 69, qty: 420 });
+	});
 
-test('ReturnedLootItem', () => {
-	const items: ReturnedLootItem[] = [
-		{ item: 1, quantity: 5 },
-		{ item: 1, quantity: 5 },
-		{ item: 1, quantity: 0 },
-		{ item: 2, quantity: 10 }
-	];
-	const bank = new Bank().add(items);
-	expect(bank.bank).toEqual({ 1: 10, 2: 10 });
-	bank.remove(items);
-	expect(bank.bank).toEqual({});
-	expect(bank.amount(1)).toBe(0);
-	expect(bank.amount(2)).toBe(0);
-});
+	test('ReturnedLootItem', () => {
+		const items: ReturnedLootItem[] = [
+			{ item: 1, quantity: 5 },
+			{ item: 1, quantity: 5 },
+			{ item: 1, quantity: 0 },
+			{ item: 2, quantity: 10 }
+		];
+		const bank = new Bank().add(items);
+		expect(bank.bank).toEqual({ 1: 10, 2: 10 });
+		bank.remove(items);
+		expect(bank.bank).toEqual({});
+		expect(bank.amount(1)).toBe(0);
+		expect(bank.amount(2)).toBe(0);
+	});
 
-test('other', () => {
-	const bank = new Bank().add(1).add(1).add(1).add(1);
+	test('other', () => {
+		const bank = new Bank().add(1).add(1).add(1).add(1);
 
-	expect(bank.amount(1)).toBe(4);
+		expect(bank.amount(1)).toBe(4);
 
-	bank.remove({ 1: 4 });
-	expect(bank.amount(1)).toBe(0);
+		bank.remove({ 1: 4 });
+		expect(bank.amount(1)).toBe(0);
 
-	bank.add({ Toolkit: 4 });
-	expect(bank.amount(1)).toBe(4);
+		bank.add({ Toolkit: 4 });
+		expect(bank.amount(1)).toBe(4);
 
-	bank.remove({ Toolkit: 4 });
-	expect(bank.amount(1)).toBe(0);
+		bank.remove({ Toolkit: 4 });
+		expect(bank.amount(1)).toBe(0);
 
-	bank.add(TestLootTable.roll());
-	expect(bank.amount(1)).toBe(1);
+		bank.add(TestLootTable.roll());
+		expect(bank.amount(1)).toBe(1);
 
-	bank.add(new Bank({ 1: 1 }));
-	expect(bank.amount(1)).toBe(2);
+		bank.add(new Bank({ 1: 1 }));
+		expect(bank.amount(1)).toBe(2);
 
-	bank.add(new Bank({ 1: 0 }));
-	expect(bank.amount(1)).toBe(2);
+		bank.add(new Bank({ 1: 0 }));
+		expect(bank.amount(1)).toBe(2);
 
-	bank.remove(new Bank({ 1: 1 }));
-	bank.add(new Bank({}));
-	bank.add({});
+		bank.remove(new Bank({ 1: 1 }));
+		bank.add(new Bank({}));
+		bank.add({});
 
-	expect(bank.amount(1)).toBe(1);
+		expect(bank.amount(1)).toBe(1);
+	});
 });
