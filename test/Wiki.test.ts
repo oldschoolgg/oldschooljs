38fc7f9954c8f2009172aa149955ed051ff371a3
QuantumlyTangled <ethoarx@gmail.com>
2020-11-10 17:21:52 +0100

    refactor: wiki

--- test/Wiki.test.ts
+++ test/Wiki.test.ts
@@ -1,29 +1,31 @@
 import { Wiki } from '../dist';
 
-test('Searching', async () => {
-	expect.assertions(2);
+describe('Wiki', () => {
+	test('Searching', async () => {
+		expect.assertions(2);
 
-	const searchResults = await Wiki.search('tbow');
-	const twistedBow = searchResults[0];
-	expect(twistedBow.title).toBe('Twisted bow');
+		const searchResults = await Wiki.search('tbow');
+		const twistedBow = searchResults[0];
+		expect(twistedBow.title).toEqual('Twisted bow');
 
-	expect(searchResults.length).toBe(2);
-});
+		expect(searchResults.length).toEqual(2);
+	});
 
-test('Random', async () => {
-	expect.assertions(2);
+	test('Random', async () => {
+		expect.assertions(2);
 
-	const fiveRandom = await Wiki.random(5);
-	const twentyRandom = await Wiki.random(20);
+		const fiveRandom = await Wiki.random(5);
+		const twentyRandom = await Wiki.random(20);
 
-	expect(fiveRandom.length).toBe(5);
-	expect(twentyRandom.length).toBe(20);
-});
+		expect(fiveRandom.length).toEqual(5);
+		expect(twentyRandom.length).toEqual(20);
+	});
 
-test('Fetching by ID', async (done) => {
-	expect.assertions(1);
+	test('Fetching by ID', async () => {
+		expect.assertions(1);
 
-	const twistedBowPage = await Wiki.fetchPage(82098);
-	if (!twistedBowPage) return done.fail('Failed to fetch page');
-	expect(twistedBowPage.title).toBe('Twisted bow');
+		const twistedBowPage = await Wiki.fetchPage(82098);
+		if (!twistedBowPage) return fail('Failed to fetch page');
+		expect(twistedBowPage.title).toEqual('Twisted bow');
+	});
 });
