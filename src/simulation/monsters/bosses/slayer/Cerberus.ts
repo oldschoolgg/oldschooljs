cabc45a0170ac0e688e7a04d8c2444237f37afa6
GC <30398469+gc@users.noreply.github.com>
2021-05-26 02:08:01 +1000

    add slayer bosses (#185)
    * add slayer bosses
    
    * fix test

--- src/simulation/monsters/bosses/slayer/Cerberus.ts
+++ src/simulation/monsters/bosses/slayer/Cerberus.ts
@@ -1,6 +1,6 @@
-import LootTable from '../../../structures/LootTable';
-import SimpleMonster from '../../../structures/SimpleMonster';
-import RareDropTable from '../../subtables/RareDropTable';
+import LootTable from '../../../../structures/LootTable';
+import SimpleMonster from '../../../../structures/SimpleMonster';
+import RareDropTable from '../../../subtables/RareDropTable';
 
 const CerberusUniqueTable = new LootTable()
 	.add('Primordial crystal')
