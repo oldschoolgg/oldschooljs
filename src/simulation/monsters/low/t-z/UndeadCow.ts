facea4baa7fa29438a3024467e40259c9c7b74df
themrrobert <10122432+themrrobert@users.noreply.github.com>
2021-06-11 08:22:19 -0700

    Fix drop tables

--- src/simulation/monsters/low/t-z/UndeadCow.ts
+++ src/simulation/monsters/low/t-z/UndeadCow.ts
@@ -1,7 +1,7 @@
 import LootTable from '../../../../structures/LootTable';
 import SimpleMonster from '../../../../structures/SimpleMonster';
 
-export const UndeadCowTable = new LootTable().every('Bones').every('Cowhide').every('Raw beef');
+export const UndeadCowTable = new LootTable().every('Bones').every('Cowhide').every(4287);
 
 export default new SimpleMonster({
 	id: 2992,
