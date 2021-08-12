1e521177e7933e47e536dbbcb39547c007cd254e
Genet Schneider <genet.schneider@pipedrive.com>
2021-08-12 19:00:12 +0300

    Export more types

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
 import * as constants from './constants';
 import { MonsterSlayerMaster } from './meta/monsterData';
 import { MonsterKillOptions } from './meta/types';
+import * as Types from './meta/types';
 import * as Clues from './simulation/clues';
 import Minigames from './simulation/minigames';
 import * as Misc from './simulation/misc';
 import Monsters from './simulation/monsters/index';
 import Openables from './simulation/openables/index';
 import Bank from './structures/Bank';
+import Clue from './structures/Clue';
 import Hiscores from './structures/Hiscores';
 import Items from './structures/Items';
 import LootTable from './structures/LootTable';
+import Monster from './structures/Monster';
 import News from './structures/News';
 import Player from './structures/Player';
 import Polls from './structures/Polls';
+import SimpleMonster from './structures/SimpleMonster';
 import Wiki from './structures/Wiki';
 import Worlds from './structures/Worlds';
 import * as Util from './util/';
@@ -27,6 +31,7 @@ export {
 	Wiki,
 	Polls,
 	Clues,
+	Clue,
 	Misc,
 	Monsters,
 	Minigames,
@@ -35,5 +40,8 @@ export {
 	LootTable,
 	constants,
 	MonsterKillOptions,
-	MonsterSlayerMaster
+	MonsterSlayerMaster,
+	Types,
+	Monster,
+	SimpleMonster
 };
