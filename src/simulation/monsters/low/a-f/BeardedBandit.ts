306daea8595989cb32f9aaccb568084874b373c4
themrrobert <10122432+themrrobert@users.noreply.github.com>
2021-08-25 02:21:36 -0700

    Move Rocky to the bandit's pickpocket table

--- src/simulation/monsters/low/a-f/BeardedBandit.ts
+++ src/simulation/monsters/low/a-f/BeardedBandit.ts
@@ -4,10 +4,7 @@ import SimpleMonster from '../../../../structures/SimpleMonster';
 export default new SimpleMonster({
 	id: 736,
 	name: 'Bearded Pollnivnian Bandit',
-	table: new LootTable({ limit: 5 })
-		.every('Bones')
-		.add('Coins', [10, 300])
-		.tertiary(257_211, 'Rocky'),
-	pickpocketTable: new LootTable().add('Coins', 40),
-	aliases: ['bearded pollnivnian bandit', 'pollnivnian bandit']
+	table: new LootTable({ limit: 5 }).every('Bones').add('Coins', [10, 300]),
+	pickpocketTable: new LootTable().add('Coins', 40).tertiary(257_211, 'Rocky'),
+	aliases: ['bearded pollnivnian bandit', 'pollnivnian bandit', 'bearded bandit']
 });
