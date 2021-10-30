02463a930c45abd8fc46b0fbf3320eaa2ee25de1
GC <30398469+gc@users.noreply.github.com>
2021-10-31 05:21:01 +1100

    Phosani (#265)
    * update
    
    * add phosani

--- src/simulation/misc/Nightmare.ts
+++ src/simulation/misc/Nightmare.ts
@@ -18,48 +18,93 @@ export interface NightmareOptions {
 	 * The members of the team killing the nightmare.
 	 */
 	team: TeamMember[];
+	isPhosani: boolean;
 }
 
 const data: Record<string, [number[], number]> = {
 	// [name, [quantity_range, weighting]]
 
 	// Runes and ammunition
-	'Cosmic rune': [[15, 214], 4],
-	'Nature rune': [[6, 138], 4],
-	'Death rune': [[24, 165], 4],
+	'Cosmic rune': [[15, 218], 4],
+	'Nature rune': [[6, 165], 4],
+	'Death rune': [[24, 176], 4],
 	'Blood rune': [[13, 129], 4],
-	'Soul rune': [[12, 74], 4],
-	'Adamant arrow': [[32, 488], 3],
-	'Rune arrow': [[12, 515], 3],
+	'Soul rune': [[12, 126], 4],
+	'Adamant arrow': [[32, 533], 3],
+	'Rune arrow': [[12, 539], 3],
+	Cannonball: [[72, 192], 2],
 
 	// Resources
-	'Yew logs': [[14, 111], 4],
-	'Magic logs': [[3, 50], 4],
-	'Gold ore': [[14, 79], 4],
+	'Yew logs': [[14, 121], 4],
+	'Magic logs': [[3, 55], 4],
+	'Gold ore': [[14, 174], 4],
 	Coal: [[16, 253], 4],
-	'Mithril ore': [[15, 69], 4],
+	'Mithril ore': [[15, 168], 4],
 	'Adamantite ore': [[8, 50], 4],
-	'Grimy cadantine': [[1, 8], 4],
+	'Grimy cadantine': [[1, 14], 4],
 	'Grimy torstol': [[1, 16], 4],
-	'Uncut emerald': [[1, 26], 3],
+	'Uncut emerald': [[1, 41], 3],
 	'Uncut ruby': [[2, 35], 3],
-	'Pure essence': [[420, 3280], 2],
+	'Snapdragon seed': [[1, 6], 1],
 
 	// Consumables
 	Shark: [[1, 15], 6],
 	Bass: [[1, 18], 6],
-	'Prayer potion(3)': [[2, 10], 5],
+	'Prayer potion(3)': [[1, 10], 5],
 	'Saradomin brew(3)': [[1, 10], 5],
-	'Zamorak brew(3)': [[1, 8], 5],
+	'Zamorak brew(3)': [[1, 10], 5],
 	'Sanfew serum(3)': [[1, 11], 5],
 
-	Coins: [[2717, 41_575], 2]
+	Coins: [[2717, 2, 43_854], 2]
 };
 const nonUniqueItemRanges = resolveNameBank(data);
 
 const NonUniqueTable = new SimpleTable<number>();
 for (const [id, data] of Object.entries(nonUniqueItemRanges)) {
 	NonUniqueTable.add(parseInt(id), data[1]);
+}
+
+// Phosani
+const phosaniData: Record<string, [number[], number]> = {
+	// [name, [quantity_range, weighting]]
+
+	// Runes and ammunition
+	'Cosmic rune': [[247, 420], 4],
+	'Nature rune': [[165, 305], 4],
+	'Death rune': [[165, 305], 4],
+	'Blood rune': [[343, 765], 4],
+	'Soul rune': [[110, 228], 4],
+	Cannonball: [[137, 382], 4],
+	'Rune arrow': [[412, 957], 3],
+
+	// Resources
+	'Mithril ore': [[165, 305], 5],
+	Coal: [[220, 458], 4],
+	'Gold ore': [[165, 305], 4],
+	'Adamantite ore': [[40, 95], 4],
+	'Magic logs': [[40, 95], 4],
+	'Grimy cadantine': [[13, 26], 4],
+	'Grimy torstol': [[13, 26], 4],
+	'Snapdragon seed': [[5, 10], 3],
+	'Uncut emerald': [[33, 75], 3],
+	'Uncut ruby': [[27, 60], 3],
+	'Runite ore': [[11, 26], 2],
+
+	// Consumables
+	Bass: [[16, 29], 6],
+	Shark: [[13, 26], 6],
+	'Prayer potion(3)': [[8, 15], 5],
+	'Sanfew serum(3)': [[6, 12], 5],
+	'Saradomin brew(3)': [[8, 15], 5],
+	'Zamorak brew(3)': [[1, 15], 5],
+
+	Coins: [[41_417, 2, 72_013], 2]
+};
+const phosaniNonUniqueItemRanges = resolveNameBank(phosaniData);
+
+const PhosaniNonUniqueTable = new SimpleTable<number>();
+for (const [id, data] of Object.entries(phosaniNonUniqueItemRanges)) {
+	PhosaniNonUniqueTable.add(parseInt(id), data[1]);
 }
 
 const GearTable = new SimpleTable<string>()
@@ -84,6 +129,13 @@ const nonMvpTertiary = new LootTable()
 	.tertiary(2000, 'Jar of dreams')
 	.tertiary(4000, 'Little nightmare');
 
+const phosaniTertiary = new LootTable()
+	.tertiary(35, 'Clue scroll (elite)')
+	.tertiary(100, 'Slepey tablet')
+	.tertiary(200, 'Parasitic egg')
+	.tertiary(4000, 'Jar of dreams')
+	.tertiary(1400, 'Little nightmare');
+
 class NightmareClass {
 	hp = 2400;
 
@@ -95,10 +147,17 @@ class NightmareClass {
 		...nonMvpTertiary.allItems
 	];
 
-	public rollNonUniqueLoot(percentage: number, isMvp: boolean): [number, number] {
-		const { item } = NonUniqueTable.roll();
+	public rollNonUniqueLoot(
+		percentage: number,
+		isMvp: boolean,
+		isPhosani: boolean
+	): [number, number] {
+		const [table, ranges] = isPhosani
+			? [PhosaniNonUniqueTable, phosaniNonUniqueItemRanges]
+			: [NonUniqueTable, nonUniqueItemRanges];
+		const { item } = table.roll();
 
-		const [range] = nonUniqueItemRanges[item];
+		const [range] = ranges[item];
 		// If the quantity range of the item is 50-100, we
 		// give you 50 qty to start, then increase it.
 		let quantity = range[0];
@@ -129,37 +188,49 @@ class NightmareClass {
 
 		const lootResult: LootBank = {};
 
-		// Construct a weighted table, where the weighting is the percent of the total HP that the team member has damaged,
-		// for example, dealing 50% off of the nightmares health will give a weighting of 50, so they're 5x more likely
-		// to get the unique than the person who dealt 10%. However, in most realistic scenarios, the team members will have
-		// done very similar amounts of damage, varying by only a few percent.
-		const WeightedUniqueTable = new SimpleTable<string>();
 		for (const teamMember of parsedTeam) {
 			lootResult[teamMember.id] = new Bank();
-
-			WeightedUniqueTable.add(teamMember.id, teamMember.percentDamage);
 		}
 
-		function giveWeightedDrop(item: string): void {
-			const { item: recipient } = WeightedUniqueTable.roll();
-			lootResult[recipient].add(item);
-		}
+		if (options.isPhosani) {
+			if (roll(200)) {
+				lootResult[options.team[0].id].add(GearTable.roll().item);
+			}
 
-		if (roll(120)) {
-			giveWeightedDrop(GearTable.roll().item);
-		}
+			if (roll(1000)) {
+				lootResult[options.team[0].id].add(OrbTable.roll().item);
+			}
+		} else {
+			// Construct a weighted table, where the weighting is the percent of the total HP that the team member has damaged,
+			// for example, dealing 50% off of the nightmares health will give a weighting of 50, so they're 5x more likely
+			// to get the unique than the person who dealt 10%. However, in most realistic scenarios, the team members will have
+			// done very similar amounts of damage, varying by only a few percent.
+			const WeightedUniqueTable = new SimpleTable<string>();
+			for (const teamMember of parsedTeam) {
+				WeightedUniqueTable.add(teamMember.id, teamMember.percentDamage);
+			}
 
-		if (roll(600)) {
-			giveWeightedDrop(OrbTable.roll().item);
-		}
+			function giveWeightedDrop(item: string): void {
+				const { item: recipient } = WeightedUniqueTable.roll();
+				lootResult[recipient].add(item);
+			}
 
-		const secondRollChance = Math.min(75, parsedTeam.length - 5);
-		if (secondRollChance > 0 && percentChance(secondRollChance)) {
+			if (roll(120)) {
+				giveWeightedDrop(GearTable.roll().item);
+			}
+
 			if (roll(600)) {
 				giveWeightedDrop(OrbTable.roll().item);
 			}
-			if (roll(120)) {
-				giveWeightedDrop(GearTable.roll().item);
+
+			const secondRollChance = Math.min(75, parsedTeam.length - 5);
+			if (secondRollChance > 0 && percentChance(secondRollChance)) {
+				if (roll(600)) {
+					giveWeightedDrop(OrbTable.roll().item);
+				}
+				if (roll(120)) {
+					giveWeightedDrop(GearTable.roll().item);
+				}
 			}
 		}
 
@@ -167,12 +238,20 @@ class NightmareClass {
 		for (const teamMember of parsedTeam) {
 			if (Object.keys(lootResult[teamMember.id].bank).length === 0) {
 				lootResult[teamMember.id].add(
-					...this.rollNonUniqueLoot(teamMember.scaledPercentDamage, teamMember.mvp)
+					...this.rollNonUniqueLoot(
+						teamMember.scaledPercentDamage,
+						teamMember.mvp,
+						options.isPhosani
+					)
 				);
 			}
 			lootResult[teamMember.id].add(teamMember.mvp ? 'Big bones' : 'Bones');
 			lootResult[teamMember.id].add(
-				teamMember.mvp ? mvpTertiary.roll() : nonMvpTertiary.roll()
+				options.isPhosani
+					? phosaniTertiary.roll()
+					: teamMember.mvp
+					? mvpTertiary.roll()
+					: nonMvpTertiary.roll()
 			);
 		}
 
