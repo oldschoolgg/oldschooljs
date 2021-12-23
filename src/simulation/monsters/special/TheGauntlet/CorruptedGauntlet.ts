ff736c0e847081afca4e02f899011d082e02bf17
Kenneth Schlappkohl <kschlappkohl@gmail.com>
2021-12-23 08:31:27 -0700

    Update CG simple monster name and id

--- src/simulation/monsters/special/TheGauntlet/CorruptedGauntlet.ts
+++ src/simulation/monsters/special/TheGauntlet/CorruptedGauntlet.ts
@@ -2,8 +2,8 @@ import { CorruptedGauntletTable } from './CorruptedGauntlet.table'
 import SimpleMonster from '../../../../structures/SimpleMonster'
 
 export default new SimpleMonster({
-	id: 0,
-	name: '',
+	id: 9035,
+	name: 'Corrupted Hunllef',
 	table: CorruptedGauntletTable,
 	aliases: ['corrupted gauntlet', 'cg', 'cgaunt']
 })
