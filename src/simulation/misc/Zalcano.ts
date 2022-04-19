4682675246691eb94151e40699c579feb350706f
TastyPumPum <james.selfe@hotmail.co.uk>
2022-04-19 22:37:47 +0100

    Lint

--- src/simulation/misc/Zalcano.ts
+++ src/simulation/misc/Zalcano.ts
@@ -66,11 +66,8 @@ const tertiaryTable = new LootTable()
 	.tertiary(1125, 'Zalcano shard');
 
 class ZalcanoClass {
-	allItems: number[] = [
-		...tertiaryTable.allItems,
-		...data.allItems
-	];
-	
+	allItems: number[] = [...tertiaryTable.allItems, ...data.allItems];
+
 	public rollNonUniqueLoot(perfPercent: number, isMVP: boolean): [number, number] {
 		const { item } = NonUniqueTable.roll();
 
