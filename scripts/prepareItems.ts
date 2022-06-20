f240d80652926f85b5ff130a11928436521bcf19
gc <30398469+gc@users.noreply.github.com>
2022-06-20 14:29:32 +1000

    v2.1.51 - price changes

--- scripts/prepareItems.ts
+++ scripts/prepareItems.ts
@@ -96,7 +96,6 @@ const itemsToIgnorePrices = [
 	'Tribal mask',
 	'White dagger',
 	'Sandstone (5kg)',
-	'Egg and tomato',
 	'Minced meat',
 	'Bagged dead tree',
 	'Kitchen table',
@@ -109,7 +108,21 @@ const itemsToIgnorePrices = [
 	'Steel hasta',
 	'Defence mix(2)',
 	'Arceuus banner',
-	'Yellow cape'
+	'Yellow cape',
+	"Iron spear(p+)",
+	'Egg and tomato',
+	"Druid's robe top",
+	"Bronze thrownaxe",
+	"Steel thrownaxe",
+	"Iron javelin(p)",
+	"Black longsword",
+	"Premade s'y crunch",
+	"Redberry pie", "Serum 207 (2)",
+	"Fremennik red shirt", "Dwellberry seed", "Iron javelin(p++)", "Steel knife(p+)", "Mithril dagger(p++)",
+	"Thatch spar dense", "Villager armband", "Sliced mushrooms", "Raw fish pie", "Blue d'hide chaps (g)", "Oak armchair",
+	"Teak armchair", "Beer barrel", "Carved oak bench", "Carved teak bench", "Oak stock", "Oak toy box", "Teak magic wardrobe",
+	"Oak armour case", "Roast beast meat", "Relicym's mix(2)", "Antidote+ mix(1)",
+
 ]
 	.map(getItem)
 	.map((i) => i.id);
@@ -199,23 +212,32 @@ export default async function prepareItems(): Promise<void> {
 		if (item.id === 995) {
 			item.price = 1;
 		}
-		if (itemsToIgnorePrices.includes(item.id)) {
+		if (itemsToIgnorePrices.includes(item.id) || !item.tradeable) {
 			item.price = 0;
 		}
 
-		if (previousItem && item.tradeable && previousItem.price < item.price / 100) {
+		// If major price increase, just dont fucking change it.
+		if (previousItem && item.tradeable && previousItem.price < item.price / 20) {
 			majorPriceChanges.push([previousItem, item]);
+			item.price = previousItem.price;
 		}
 
-		// Dont change price if its only a <3% difference
+		// Dont change price if its only a <10% difference and price is less than 100k
 		if (
+			previousItem &&
+			item.price > reduceNumByPercent(previousItem?.price, 10) &&
+			item.price < increaseNumByPercent(previousItem?.price, 10) &&
+			item.price < 100_000
+		) {
+			item.price = previousItem.price;
+		} else if (
+			// Ignore <3% changes in any way
 			previousItem &&
 			item.price > reduceNumByPercent(previousItem?.price, 3) &&
 			item.price < increaseNumByPercent(previousItem?.price, 3)
 		) {
 			item.price = previousItem.price;
 		}
-
 		itemNameMap[item.id] = item;
 	}
 
@@ -223,7 +245,7 @@ export default async function prepareItems(): Promise<void> {
 	console.log(`Deleted Items: ${moidLink(deletedItems)}.`);
 
 	console.log(
-		`Major price changes: ${majorPriceChanges
+		`Major price changes NOT changed: ${majorPriceChanges
 			.map((ent) => `${ent[0].name} (${ent[0].price} to ${ent[1].price})`)
 			.join(', ')}.`
 	);
