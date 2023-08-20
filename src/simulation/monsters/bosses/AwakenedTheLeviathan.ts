5c513e13168d1aa00ff7c974f314102fb153e2d9
GC <30398469+gc@users.noreply.github.com>
2023-08-21 01:44:33 +1000

    Add DT2 bosses (#345)

---
+++ src/simulation/monsters/bosses/AwakenedTheLeviathan.ts
@@ -0,0 +1,16 @@
+import { MonsterKillOptions } from '../../../meta/types';
+import Bank from '../../../structures/Bank';
+import Monster from '../../../structures/Monster';
+import { TheLeviathan } from './TheLeviathan';
+
+class AwakenedTheLeviathanSingleton extends Monster {
+	public kill(quantity = 1, options: MonsterKillOptions = {}): Bank {
+		return TheLeviathan.kill(quantity, { ...options, isAwakened: true });
+	}
+}
+
+export const AwakenedTheLeviathan = new AwakenedTheLeviathanSingleton({
+	id: 12_215,
+	name: 'The Leviathan (Awakened)',
+	aliases: ['the leviathan (awakened)']
+});
---
+++ src/simulation/monsters/bosses/AwakenedVardorvis.ts
@@ -0,0 +1,16 @@
+import { MonsterKillOptions } from '../../../meta/types';
+import Bank from '../../../structures/Bank';
+import Monster from '../../../structures/Monster';
+import { Vardorvis } from './Vardorvis';
+
+class AwakenedVardorvisSingleton extends Monster {
+	public kill(quantity = 1, options: MonsterKillOptions = {}): Bank {
+		return Vardorvis.kill(quantity, { ...options, isAwakened: true });
+	}
+}
+
+export const AwakenedVardorvis = new AwakenedVardorvisSingleton({
+	id: 12_224,
+	name: 'Vardorvis (Awakened)',
+	aliases: ['vardorvis (awakened)']
+});
