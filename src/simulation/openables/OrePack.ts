601b2140e4006864dafd308dae192ba7002775cf
nwjgit <nwjgit@gmail.com>
2024-01-19 11:14:49 -0600

    orepack.ts

--- src/simulation/openables/OrePack.ts
+++ src/simulation/openables/OrePack.ts
@@ -1,18 +1,38 @@
+import { randInt } from 'e';
+
 import LootTable from '../../structures/LootTable';
 import SimpleOpenable from '../../structures/SimpleOpenable';
 
-const OreTable = new LootTable()
-	.add('Coal', 1, 181)
-	.add('Iron ore', 1, 25)
-	.add('Mithril ore', 1, 17)
-	.add('Adamantite ore', 1, 3)
-	.add('Runite ore', 1, 1);
+// Giant's Foundry Ore pack
+const GFOrePackTable = new LootTable()
+	.add('Coal', 1, 81.27)
+	.add('Iron ore', 1, 10.94)
+	.add('Mithril ore', 1, 6.24)
+	.add('Adamantite ore', 1, 1.29)
+	.add('Runite ore', 1, 0.26);
 
-const OrePackTable = new LootTable().every(OreTable, 30);
-
-export default new SimpleOpenable({
+export const GiantsFoundryOrePack = new SimpleOpenable({
 	id: 27_019,
 	name: 'Ore pack',
 	aliases: ['ore pack'],
-	table: OrePackTable
+	table: new LootTable().every(GFOrePackTable, 30)
 });
+
+// Volcanic Mine Ore pack
+const VMOrePackTable = new LootTable()
+	.add('Iron ore', 1, 47.46)
+	.add('Coal', 1, 14.05)
+	.add('Silver ore', 1, 12.7)
+	.add('Gold ore', 1, 10.15)
+	.add('Mithril ore', 1, 8.76)
+	.add('Adamantite ore', 1, 3.65)
+	.add('Runite ore', 1, 0.98);
+
+export const VolcanicMineOrePack = new SimpleOpenable({
+	id: 27_693,
+	name: 'Ore pack',
+	aliases: ['ore pack'],
+	table: new LootTable().oneIn(100, 'Dragon pickaxe (broken)').every(VMOrePackTable, randInt(38, 43))
+});
+
+export const OrePacks = [GiantsFoundryOrePack, VolcanicMineOrePack];
