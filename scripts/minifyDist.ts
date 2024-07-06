9679e9d2084be1928681f8ca77067da85042805b
gc <30398469+gc@users.noreply.github.com>
2024-07-07 08:58:24 +1000

    2.5.9 undo path changes

--- scripts/minifyDist.ts
+++ scripts/minifyDist.ts
@@ -1,23 +1,13 @@
-import { promises as fs } from "node:fs";
-import * as path from "node:path";
+import { readFileSync, writeFileSync } from "node:fs";
+import { join } from "node:path";
 
-async function walk(dir: string, fileList: string[] = []): Promise<string[]> {
-	const files = await fs.readdir(dir);
-	for (const file of files) {
-		const stat = await fs.stat(path.join(dir, file));
-		if (stat.isDirectory()) fileList = await walk(path.join(dir, file), fileList);
-		else fileList.push(path.join(dir, file));
-	}
-	return fileList;
-}
-
-export default async function minifyDist(): Promise<void> {
-	const arrayOfPaths = await walk("./dist");
-	for (const path of arrayOfPaths) {
-		if (path.endsWith(".json")) {
-			const jsonString = await fs.readFile(path).then((file): string => file.toString());
-			const minifiedJson = JSON.stringify(JSON.parse(jsonString));
-			await fs.writeFile(path, minifiedJson);
-		}
+function minifyDist() {
+	for (const path of [join("dist", "data", "monsters_data.json"), join("dist", "data", "items", "item_data.json")]) {
+		const jsonString = readFileSync(path).toString();
+		const minifiedJson = JSON.stringify(JSON.parse(jsonString));
+		writeFileSync(path, minifiedJson);
+		console.log(`Minifying ${path}`);
 	}
 }
+
+minifyDist();
--- scripts/prepare.ts
+++ scripts/prepare.ts
@@ -1,5 +1,3 @@
-import minifyDist from "./minifyDist";
 import prepareItems from "./prepareItems";
 
-minifyDist();
 prepareItems();
