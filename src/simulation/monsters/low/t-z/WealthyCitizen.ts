6fa049b9bcfc98ff66283e8f13d95e12c2d3d629
DayV <Ashleyjd22@gmail.com>
2024-11-08 20:23:40 +0000

    loot table fix

--- src/simulation/monsters/low/t-z/WealthyCitizen.ts
+++ src/simulation/monsters/low/t-z/WealthyCitizen.ts
@@ -4,9 +4,9 @@ import SimpleMonster from "../../../../structures/SimpleMonster";
 export default new SimpleMonster({
 	id: 13302,
 	name: "Wealthy Citizen",
-	pickpocketTable: new LootTable()
-		.add("Coins", 85, 1 / 1.076)
-		.add("House keys", 1, 1 / 17)
-		.add("Clue Scroll (Easy)", 1, 1 / 85),
-	aliases: ["wealthy citizen", "wealthy", "varlamore"],
+	pickpocketTable: new LootTable({ limit: 1445 })
+		.add("Coins", 85, 1343)
+		.add("House keys", 1, 85)
+		.add("Clue Scroll (Easy)", 1, 17),
+	aliases: ["wealthy citizen", "wealthy", "citizen", "varlamore citizen"],
 });
